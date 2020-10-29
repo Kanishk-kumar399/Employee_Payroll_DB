3e459f623bb2f027a10e75a24a233c6873d69392
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-29 13:33:13 +0530

    [Kanishk] added:ability to add records in the table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -23,3 +23,9 @@ CREATE TABLE employee_payroll
 	);
 #display table
 DESCRIBE employee_payroll;
+
+#Inserting records in table
+ INSERT INTO employee_payroll (name,salary,start) VALUES
+    ('BILL',1000000.00,'2018-01-03'),
+    ('Terisa',2000000.00,'2019-11-13'),
+    ('Charlie',3000000.00,'2020-05-21');

d5f68393cbc326d807d7511a25fee1e6c3a765db
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-29 13:23:47 +0530

    [Kanishk] added:ability to use ,show and select database

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,1 +1,14 @@
 #Payroll_Service
+
+#See existing databases
+show databases;
+
+#Creating a new database
+create database payroll_service;
+
+#To work on a database
+USE payroll_service;
+
+#To select database
+SELECT DATABASE();
+
